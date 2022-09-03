6d05414fc36e2d2b93530d17ea4b17fdafb14119
diegolopez-ing <diegolopez.coder@gmail.com>
2022-09-03 16:44:20 -0500

    Type conversion

--- conversion.go
+++ conversion.go
@@ -15,8 +15,9 @@ func main() {
 
 	b = 1000
 	fmt.Println(b)
-	fmt.Printf("%T\n", a)
-
+	fmt.Printf("%T\n", b)
+	//a = b   cannot use b (variable of type dinero) as int value in assignment
+	// Converting the value of b from dinero to int.
 	a = int(b)
 	fmt.Println(a)
 	fmt.Printf("%T\n", a)
